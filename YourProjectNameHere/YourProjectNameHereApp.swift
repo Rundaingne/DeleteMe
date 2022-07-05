faa9df982be22af342c175666ea1813789dcdaab
Rundaingne <48364843+Rundaingne@users.noreply.github.com>
2022-07-05 17:23:23 -0600

    Initial Commit

---
+++ YourProjectNameHere/YourProjectNameHereApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourProjectNameHereApp.swift
+//  YourProjectNameHere
+//
+//  Created by Erich Kumpunen on 7/5/22.
+//
+
+import SwiftUI
+
+@main
+struct YourProjectNameHereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
